39fea7641656395ea524421345765db9e6853eb6
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-20 20:07:22 +0000

    dd

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var app = server();
 var moment = require('moment');
 var fs = require('fs');
 var path = require('path');
-
 var port = process.env.PORT || 3500;
 
 app.listen(port, function(){
   console.log("Listening on port: " + port);
 });
 
+//landing page default: Index.html
 app.get('/', function(req, res) {
 var fileName = path.join(__dirname, 'index.html');
   res.sendFile(fileName, function (err) {
@@ -23,14 +23,18 @@ var fileName = path.join(__dirname, 'index.html');
   });
 });
 
+//endpoints for Unix & Natural
 app.get('/:datestring', function(req,res) {
   var myDate;
+  //case if Unix
   if(/^\d{8,}$/.test(req.params.datestring)) {
     myDate = moment(req.params.datestring, "X");
-  } else {
+    
+  } else { //case if Natural
     myDate = moment(req.params.datestring, "MMMM D, YYYY");
   }
 
+//check for errors
   if(myDate.isValid()) {
     res.json({
       unix: myDate.format("X"),
