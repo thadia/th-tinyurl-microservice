7753ea1d1bd8bfea5791e38ac62cfe05511ea5b1
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-21 20:48:58 +0000

    initial

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ app.listen(port, function(){
 
 app.get('/new/:inputurl', function(req,res) {
   var result, inputurl;
-  
-  result = shrink.shorten(inputurl);
+   result = shrink.shorten(inputurl);
  
     res.json({
         
       //{ "original_url":"http://foo.com:80", "short_url":"https://little-url.herokuapp.com/8170" }
      
       original_url:result.longUrl, //=> 'http://google.com'
-      short_url:result.shortUrl //=> 'http://mt.cx/AAA'
+      short_url:result.shortUrl,
+      original:inputurl //=> 'http://mt.cx/AAA'
     });
   
   
