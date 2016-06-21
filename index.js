04bbb7e751ea86efbed919ec58a3f33000651215
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-21 16:19:56 +0000

    adding

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var platform = require('platform');
 var server = require('express');
-//var moment = require('moment');
-//var path = require('path');
 var port = process.env.PORT || 3500;
 var app = server();
 
@@ -9,11 +6,9 @@ app.listen(port, function(){
   console.log('Ready: ' + port);
   });
 
-//endpoints for Unix & Natural
 app.get('/api/whoami', function(req,res) {
   
     res.json({
-      
      //{"ipaddress":"216.64.167.250","language":"en-US","software":"Windows NT 6.1; WOW64; rv:47.0"}
       ipaddress:req.headers['x-forwarded-for'] || 
                 req.connection.remoteAddress || 
@@ -22,9 +17,6 @@ app.get('/api/whoami', function(req,res) {
       language:req.headers['accept-language'],
       software:req.headers['user-agent']
 
-      
-      //unix: outputdata.format("X"),
-      //natural: outputdata.format("MMMM D, YYYY")
     });
   
   
