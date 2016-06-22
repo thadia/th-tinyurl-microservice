cc5a262d7a2504bb0641a2e16e6783daf0070d94
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-22 20:12:32 +0000

    adding

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var server = require('express');
 var port = process.env.PORT || 3500;
 var app = server();
-var map_global=[[]];
+var map_global=[[,],[,],[,]];
 var tiny_global=0;
 var max =100;
 var c=0; 
@@ -29,7 +29,7 @@ app.get('/new/:inputurl(*)/', function(req,res) {
 
 app.get('/:tiny', function(req,res) {
    //do a for  
-   console.log("Logging::::" + req.params.tiny + "::::::"  + map_global);
+   console.log("Logging::::" + req.params.tiny + "::::::"  + map_global + "::::::" map_global.length);
 
    var redir_url=search.findTiny(req.params.tiny,map_global);
    console.log("Logging::::" + redir_url);
