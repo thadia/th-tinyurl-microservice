a8f2791698f1329481d173120fa8b239d91cc4a5
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-22 19:14:48 +0000

    adding

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var port = process.env.PORT || 3500;
 var app = server();
 var map_global=[[]]; 
 var max =100;
+var c=0; var tinyCount=0;
+var search = require('./search.js');
 
 app.listen(port, function(){ 
   console.log('Ready: ' + port);
@@ -13,16 +15,17 @@ app.get('/new/:inputurl(*)/', function(req,res) {
     res.json({
       //{ "original_url":"http://foo.com:80", "short_url":"https://little-url.herokuapp.com/8170" }
       original_url:req.params.inputurl, //=> 'http://google.com'
-      short_url:"https://th-tinyurl-microservice.herokuapp.com/" + "1313"
+      short_url:"https://th-tinyurl-microservice.herokuapp.com/" + tinyCount++
     });
     
-    map_global[0][1] = req.params.inputurl;
-    map_global[0][0] = "https://th-tinyurl-microservice.herokuapp.com/" + "1313";
+    map_global[c][1] = req.params.inputurl;
+    map_global[c][0] = tinyCount;
 
 });
 
-app.get('/1313', function(req,res) {
-
-    res.redirect(map_global[0][1]);
-
+app.get('/:tiny', function(req,res) {
+   //do a for
+   var redir_url=search(req.params.tiny,map_global);
+   res.redirect(redir_url);
+   
 });
