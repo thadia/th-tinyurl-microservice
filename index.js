63426923fb0af9c1f0381b38cb4e762c9c0116c0
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-24 15:41:15 +0000

    fixing

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.listen(port, function(){
   });
 
 app.get('/new/:inputurl(*)/', function(req,res) {
-  if(tiny_global >= max) map_global.length=0;
+  if(tiny_global >= max) { map_global.length=0; tiny_global=0 }
   
     res.json({
       //{ "original_url":"http://foo.com:80", "short_url":"https://little-url.herokuapp.com/8170" }
